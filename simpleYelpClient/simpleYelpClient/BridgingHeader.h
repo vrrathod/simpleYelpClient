96ad7b85a7c7f766f027112f3d194cc153804501
vr <vr@kopi-luwak.local>
2014-09-21 13:58:12 -0700

    added bridging header

---
+++ simpleYelpClient/simpleYelpClient/BridgingHeader.h
@@ -0,0 +1,20 @@
+//
+//  BridgingHeader.h
+//  simpleYelpClient
+//
+//  Created by vr on 9/21/14.
+//  Copyright (c) 2014 vr. All rights reserved.
+//
+
+#ifndef simpleYelpClient_BridgingHeader_h
+#define simpleYelpClient_BridgingHeader_h
+
+
+// Import AFNetworking headers
+#import "AFNetworking.h"
+#import "UIImageView+AFNetworking.h"
+
+
+// Import BDBOAuth1
+
+#endif
