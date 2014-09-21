2d5ae6e50c8552b2a80d9d8bc1a2d89e33408a26
vr <vr@kopi-luwak.local>
2014-09-21 15:39:39 -0700

    hooked data source for table view

--- simpleYelpClient/simpleYelpClient/ViewController.swift
+++ simpleYelpClient/simpleYelpClient/ViewController.swift
@@ -8,12 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    // Client object for making Yelp queries.
     var client:YelpClient = YelpClient(consumerKey: YelpConstants.CONSUMER_KEY(),
                                        consumerSecret: YelpConstants.CONSUMER_SECRET(),
                                        accessToken: YelpConstants.ACCESS_TOKEN(),
                                        accessSecret: YelpConstants.ACCESS_SECRET() );
+    
+    /// contents of the table is stored in following array
+    var tableContents: NSArray? ;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +28,24 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //----------------------------------------------------------------------------
+    // Table View Data Source
+    //----------------------------------------------------------------------------
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if nil != tableContents  {
+            return tableContents!.count;
+        } else {
+            return 0;
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("vr.codepath.yelp.cell" ) as? UITableViewCell;
+        return cell!;
+    }
 
+    //----------------------------------------------------------------------------
 
 }
 
