76757d46441f6995acdd03ee9bfabc1602d25807
vr <vr@dhcp119-office44.mv.pgp.com>
2014-09-23 17:24:54 -0700

    cleaned up filter view

--- simpleYelpClient/simpleYelpClient/FilterViewController.swift
+++ simpleYelpClient/simpleYelpClient/FilterViewController.swift
@@ -23,31 +23,10 @@ class FilterViewController: UIViewController {
     
     //----------------------------------------------------------------------------
     
-    @IBOutlet weak var segSortBy: UISegmentedControl!
-    @IBOutlet weak var segRadius: UISegmentedControl!
-    @IBOutlet weak var switchOfferingDeals: UISwitch!
     var searchButton:UIBarButtonItem?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        switch ( filterSettings.sort ) {
-        case .eYelpSortBestMatch: segSortBy.selectedSegmentIndex = 0;
-        case .eYelpSortDistance: segSortBy.selectedSegmentIndex = 1;
-        case .eYelpSortHighestRated: segSortBy.selectedSegmentIndex = 2;
-        default: segSortBy.selectedSegmentIndex = 0;
-        }
-        
-        switch ( filterSettings.radius ){
-        case .eYelpRadiusHere: segRadius.selectedSegmentIndex = 0;
-        case .eYelpRadiusWalking: segRadius.selectedSegmentIndex = 1;
-        case .eYelpRadiusBiking: segRadius.selectedSegmentIndex = 2;
-        case .eYelpRadiusDriving: segRadius.selectedSegmentIndex = 3;
-        default: segRadius.selectedSegmentIndex = 0;
-        }
-        
-        switchOfferingDeals.on = filterSettings.offeringDeals;
         
         // Search button on the bar
         searchButton = UIBarButtonItem(title: "Search", style: UIBarButtonItemStyle.Bordered, target: self, action: "searchClicked:");
@@ -58,19 +37,7 @@ class FilterViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func sortingChanged(sender: AnyObject) {
-        filterSettings.setSortValue( sender.selectedSegmentIndex );
-    }
-    
-    @IBAction func distancePrefChanged(sender: AnyObject) {
-        filterSettings.setRadiusWithSegmentIndex(sender.selectedSegmentIndex);
-    }
-    
-    @IBAction func offerringDealsChanged(sender: AnyObject) {
-        filterSettings.offeringDeals = (sender as UISwitch).on;
-    }
-    
+        
     func searchClicked( sender: AnyObject ) {
         filterDelegate?.updateSettings(filterSettings);
         self.navigationController?.popViewControllerAnimated(true);
